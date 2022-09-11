9b0d86f6ef2f8cb4b2046c117d4e66f9849800ae
shzxcv <sh.zxcv00@gmail.com>
2022-09-12 00:37:05 +0900

    gorilla mux

---
+++ handler/handler.go
@@ -0,0 +1,57 @@
+package handler
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func HelloHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Hello World!\n")
+	} else {
+		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
+	}
+}
+
+func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Article...\n")
+	} else {
+		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
+	}
+}
+
+func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Article List\n")
+	} else {
+		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
+	}
+}
+
+func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		articleID := 1
+		resString := fmt.Sprintf("Article No.%d\n", articleID)
+		io.WriteString(w, resString)
+	} else {
+		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
+	}
+}
+
+func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Nice...\n")
+	} else {
+		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
+	}
+}
+
+func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Comment...\n")
+	} else {
+		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
+	}
+}
--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/shzxcv/go-restapi/handler"
 )
 
 func main() {
-	handler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello World!\n")
-	}
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler.HelloHandler)
+	r.HandleFunc("/article", handler.PostArticleHandler)
+	r.HandleFunc("/article/list", handler.ArticleListHandler)
+	r.HandleFunc("/article/1", handler.ArticleDetailHandler)
+	r.HandleFunc("/article/nice", handler.PostNiceHandler)
+	r.HandleFunc("/comment", handler.PostCommentHandler)
 
-	http.HandleFunc("/hello", handler)
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
