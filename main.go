5a4f51341d041990f81ec85df0150e1d143bd32c
shzxcv <sh.zxcv00@gmail.com>
2022-09-13 22:05:56 +0900

    models

--- main.go
+++ main.go
@@ -5,17 +5,17 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/shzxcv/go-restapi/handler"
+	"github.com/shzxcv/go-restapi/handlers"
 )
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/hello", handler.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article", handler.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", handler.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", handler.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", handler.PostNiceHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", handler.PostCommentHandler).Methods(http.MethodPost)
+	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
